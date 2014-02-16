4a6e9bb9397368272ca37741800500f27be39af4
Jim Derry <balthisar@gmail.com>
2014-02-16 13:48:35 +0800

    Added Doxygen documentation.

---
+++ dir_47736248c69b6912f39b7e3ed6fb86bf.js
@@ -0,0 +1,6 @@
+var dir_47736248c69b6912f39b7e3ed6fb86bf =
+[
+    [ "JDBlogPostHelper.php", "JDBlogPostHelper_8php.html", [
+      [ "JDBlogPostHelper", "classJDBlogPostHelper.html", "classJDBlogPostHelper" ]
+    ] ]
+];
